52c3267db6352e2e86fe00dbc9791e6fb6a68a4e
Dr.sheriff <kiptoobethuel0@gmail.com>
2026-02-11 17:43:17 +0300

    A todo list

---
+++ Todo-list/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const [Todos,setTodos] = useState([]);
+  const [input, setInput] = useState("")
+
+  const handlesubmit = (e) =>{
+    e.preventDefault();
+    setTodos((Todos) =>
+      Todos.concat({
+        text:input,
+        id:Math.random()
+      }),
+      setInput("")
+     )
+  }
+  const remove = (id) =>{
+    setTodos((Todos) => Todos.filter((t) => t.id !== id))
+  }
+  return (
+    <div className='container'>
+      <form onSubmit={handlesubmit} >
+        <input type="text"  placeholder='New Todo' value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <button type='submit'>add</button>
+      </form>
+      <div className="todo-table">
+        {
+          Todos.map((item) =>
+            <p>{item.text} <button onClick={remove}>X</button></p>
+        
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App
